828501300a2311d2babb388f61d0eb2043e4bb7b
BMNofc <mauricio2006k@outlook.com>
2026-03-12 19:53:23 -0300

    Initial commit

---
+++ UPrincipal.pas
@@ -0,0 +1,56 @@
+unit UPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TfrmPrincipal = class(TForm)
+    btnExemplo01: TButton;
+    btnAtividade01: TButton;
+    btnExemplo02: TButton;
+    btnAtividade02: TButton;
+    procedure btnExemplo01Click(Sender: TObject);
+    procedure btnAtividade01Click(Sender: TObject);
+    procedure btnExemplo02Click(Sender: TObject);
+    procedure btnAtividade02Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+uses UExemplo01, UAtividade01, UExemplo02, UAtividade02;
+
+
+
+procedure TfrmPrincipal.btnAtividade01Click(Sender: TObject);
+begin
+  frmAtividade01.ShowModal;
+end;
+
+procedure TfrmPrincipal.btnAtividade02Click(Sender: TObject);
+begin
+  frmAtividade02.ShowModal;
+end;
+
+procedure TfrmPrincipal.btnExemplo01Click(Sender: TObject);
+begin
+  frmExemplo01.ShowModal;
+end;
+
+procedure TfrmPrincipal.btnExemplo02Click(Sender: TObject);
+begin
+  frmExemplo02.ShowModal;
+end;
+
+end.
